9b6e70af53159bce63b47bf96637328731cd33c2
Johnson Kaho Poon <kahopoon@gmail.com>
2016-03-02 10:57:32 +0800

    update AI start

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -73,74 +73,103 @@ def human_action(status)
   return true
 end
 
+def computer_startround(status)
+  if status.size % 2 != 0
+    computer_turn = number_to_gridPosition(status, (status.size*status.size + 1) / 2)
+    print computer_turn
+    if is_available(status, computer_turn)
+      status[computer_turn["row"]][computer_turn["column"]] = "X"
+      return true
+    end
+  end
+  corner = [1,status.size,status.size*2+1,status.size*status.size]
+  computer_turn = rand(4)
+  while !is_available(status, number_to_gridPosition(status, corner[computer_turn])) && corner.size > 0
+    corn.delete_at(computer_turn)
+    computer_turn = rand(4)
+  end
+  computer_turn = number_to_gridPosition(status, corner[computer_turn])
+  if is_available(status, computer_turn)
+    status[computer_turn["row"]][computer_turn["column"]] = "X"
+    return true
+  end
+  return false
+end
+
 def computer_random(status)
   computer_turn = number_to_gridPosition(status, rand(status.size * status.size)+1)
   while !is_available(status, computer_turn)
     computer_turn = number_to_gridPosition(status, rand(status.size * status.size)+1)
   end
-  status[computer_turn["row"]][computer_turn["column"]] = "X"
-  return true
+  if is_available(status, computer_turn)
+    status[computer_turn["row"]][computer_turn["column"]] = "X"
+    return true
+  end
+  return false
 end
 
 def computer_AI(status)
-  side = "X"
-  opp = "O"
-  opp_count_limit = 2
-  self_count_least = 1
-  horizontal_selfCount, horizontal_oppCount, vertical_selfCount, vertical_oppCount = 0,0,0,0
-  horizontal_available_list, vertical_available_list = [],[]
-  for i in 0..status.size-1
-    for k in 0..status.size-1
-      status[i][k] == side ? horizontal_selfCount += 1 : horizontal_selfCount += 0
-      status[i][k] == opp ? horizontal_oppCount += 1 : horizontal_oppCount += 0
-      status[k][i] == side ? vertical_selfCount += 1 : vertical_selfCount += 0
-      status[k][i] == opp ? vertical_oppCount += 1 : vertical_oppCount += 0
-      if is_available(status, {"row" => i, "column" => k})
-        horizontal_available_list.push(k)
-      end
-      if is_available(status, {"row" => k, "column" => i})
-        vertical_available_list.push(k)
-      end
-    end
-    if (horizontal_selfCount < self_count_least && horizontal_oppCount >= opp_count_limit) && horizontal_available_list.size > 0
-      random_from_all_available = rand(horizontal_available_list.size)
-      status[i][horizontal_available_list[random_from_all_available]] = side
-      return true
-    end
-    if (vertical_selfCount < self_count_least && vertical_oppCount >= opp_count_limit) && vertical_available_list.size > 0
-      random_from_all_available = rand(vertical_available_list.size)
-      status[vertical_available_list[random_from_all_available]][i] = side
-      return true
-    end
+  if !computer_startround(status)
+    side = "X"
+    opp = "O"
+    opp_count_limit = 2
+    self_count_least = 1
     horizontal_selfCount, horizontal_oppCount, vertical_selfCount, vertical_oppCount = 0,0,0,0
-    horizontal_available_list.clear
-    vertical_available_list.clear
-  end
-  leftcross_selfCount, leftcross_oppCount, rightcross_selfCount, rightcross_oppCount = 0,0,0,0
-  leftcross_available_list, rightcross_available_list = [],[]
-  for i in 0..status.size-1
-    status[i][i] == side ? leftcross_selfCount += 1 : leftcross_selfCount += 0
-    status[i][i] == opp ? leftcross_oppCount += 1 : leftcross_oppCount += 0
-    status[i][status.size-1 - i] == side ? rightcross_selfCount += 1 : rightcross_selfCount += 0
-    status[i][status.size-1 - i] == opp ? rightcross_oppCount += 1 : rightcross_oppCount += 0
-    if is_available(status, {"row" => i, "column" => i})
-      leftcross_available_list.push(i)
+    horizontal_available_list, vertical_available_list = [],[]
+    for i in 0..status.size-1
+      for k in 0..status.size-1
+        status[i][k] == side ? horizontal_selfCount += 1 : horizontal_selfCount += 0
+        status[i][k] == opp ? horizontal_oppCount += 1 : horizontal_oppCount += 0
+        status[k][i] == side ? vertical_selfCount += 1 : vertical_selfCount += 0
+        status[k][i] == opp ? vertical_oppCount += 1 : vertical_oppCount += 0
+        if is_available(status, {"row" => i, "column" => k})
+          horizontal_available_list.push(k)
+        end
+        if is_available(status, {"row" => k, "column" => i})
+          vertical_available_list.push(k)
+        end
+      end
+      if (horizontal_selfCount < self_count_least && horizontal_oppCount >= opp_count_limit) && horizontal_available_list.size > 0
+        random_from_all_available = rand(horizontal_available_list.size)
+        status[i][horizontal_available_list[random_from_all_available]] = side
+        return true
+      end
+      if (vertical_selfCount < self_count_least && vertical_oppCount >= opp_count_limit) && vertical_available_list.size > 0
+        random_from_all_available = rand(vertical_available_list.size)
+        status[vertical_available_list[random_from_all_available]][i] = side
+        return true
+      end
+      horizontal_selfCount, horizontal_oppCount, vertical_selfCount, vertical_oppCount = 0,0,0,0
+      horizontal_available_list.clear
+      vertical_available_list.clear
     end
-    if is_available(status, {"row" => i, "column" => status.size-1 - i})
-      rightcross_available_list.push(i)
+    leftcross_selfCount, leftcross_oppCount, rightcross_selfCount, rightcross_oppCount = 0,0,0,0
+    leftcross_available_list, rightcross_available_list = [],[]
+    for i in 0..status.size-1
+      status[i][i] == side ? leftcross_selfCount += 1 : leftcross_selfCount += 0
+      status[i][i] == opp ? leftcross_oppCount += 1 : leftcross_oppCount += 0
+      status[i][status.size-1 - i] == side ? rightcross_selfCount += 1 : rightcross_selfCount += 0
+      status[i][status.size-1 - i] == opp ? rightcross_oppCount += 1 : rightcross_oppCount += 0
+      if is_available(status, {"row" => i, "column" => i})
+        leftcross_available_list.push(i)
+      end
+      if is_available(status, {"row" => i, "column" => status.size-1 - i})
+        rightcross_available_list.push(i)
+      end
     end
+    if (leftcross_selfCount < self_count_least && leftcross_oppCount >= opp_count_limit) && leftcross_available_list.size > 0
+      random_from_all_available = rand(leftcross_available_list.size)
+      status[leftcross_available_list[random_from_all_available]][leftcross_available_list[random_from_all_available]] = side
+      return true
+    end
+    if (rightcross_selfCount < self_count_least && rightcross_oppCount >= opp_count_limit) && rightcross_available_list.size > 0
+      random_from_all_available = rand(rightcross_available_list.size)
+      status[rightcross_available_list[random_from_all_available]][status.size-1 - rightcross_available_list[random_from_all_available]] = side
+      return true
+    end
+    return computer_random(status)
   end
-  if (leftcross_selfCount < self_count_least && leftcross_oppCount >= opp_count_limit) && leftcross_available_list.size > 0
-    random_from_all_available = rand(leftcross_available_list.size)
-    status[leftcross_available_list[random_from_all_available]][leftcross_available_list[random_from_all_available]] = side
-    return true
-  end
-  if (rightcross_selfCount < self_count_least && rightcross_oppCount >= opp_count_limit) && rightcross_available_list.size > 0
-    random_from_all_available = rand(rightcross_available_list.size)
-    status[rightcross_available_list[random_from_all_available]][status.size-1 - rightcross_available_list[random_from_all_available]] = side
-    return true
-  end
-  return computer_random(status) ? true : false
+  return true
 end
 
 def is_win(side, status)
